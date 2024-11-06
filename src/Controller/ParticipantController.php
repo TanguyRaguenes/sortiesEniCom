609c8f862ce8084e2489402c89b4eba8b4aea4a4
JORDI MAZURECK <jordi.mazureck2024@campus-eni.fr>
2024-11-06 11:43:32 +0100

    jordi participant

---
+++ src/Controller/ParticipantController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Participant;
+use App\Form\ParticipantFormType;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+
+class ParticipantController extends AbstractController
+{
+    #[Route('', name: 'index')]
+    public function index(): Response
+    {
+        return $this->render('participant/index.html.twig', [
+            'controller_name' => 'ParticipantController',
+        ]);
+    }
+    #[Route('/participant/create', name: 'app_participant_create')]
+    public function create(Request $request, EntityManagerInterface $entityManager): Response
+    {
+
+        $participant =new Participant();
+
+        $form = $this->createForm(ParticipantFormType::class, $participant);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $entityManager->persist($participant);
+            $entityManager->flush();
+            $this->addFlash('success', 'Participant ajoutée !');
+            return $this->redirectToRoute('app_main');
+
+
+        }else{
+            
+            $this->addFlash('error','veuillez verifer email et votre pseudo');
+
+        }
+        return $this->render("participant/edit.html.twig",[
+
+            'form'=>$form
+
+        ]);
+
+    }
+     #[Route('/participant/profil', name: 'app_participant_profil')]
+    public function profil(): Response
+    {
+        return $this->render('participant/profil.html.twig', [
+            'controller_name' => 'ParticipantController',
+        ]);
+    }
+}
---
+++ src/Entity/Participant.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ParticipantRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+
+
+#[ORM\Entity(repositoryClass: ParticipantRepository::class)]
+class Participant
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $firstName = null;
+
+    #[ORM\Column(length: 255,unique: true)]
+    private ?string $username = null;
+
+    #[ORM\Column(length: 255)]
+    #[Assert\Email(message: 'The email {{ value }} is not a valid email.',)]
+    private ?string $email = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $phone = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(string $firstName): static
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): static
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getPhone(): ?int
+    {
+        return $this->phone;
+    }
+
+    public function setPhone(?int $phone): static
+    {
+        $this->phone = $phone;
+
+        return $this;
+    }
+}
