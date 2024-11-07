f5cf36356348093021cb9e806e9d4eaab7654518
jordi <jordimazureck@gmail.com>
2024-11-07 09:18:01 +0100

    jordi participant

--- src/Repository/ParticipantRepository.php
+++ src/Repository/ParticipantRepository.php
@@ -38,21 +38,21 @@ class ParticipantRepository extends ServiceEntityRepository
 //    {
 //        return $this->findAll();
 //    }
-//
-    //    /**
-    //     * @return Participant[] Returns an array of Participant objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('p')
-    //            ->andWhere('p.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('p.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
+
+//        /**
+//         * @return Participant[] Returns an array of Participant objects
+//         */
+//        public function findByExampleField($value): array
+//        {
+//            return $this->createQueryBuilder('p')
+//                ->andWhere('p.exampleField = :val')
+//                ->setParameter('val', $value)
+//                ->orderBy('p.id', 'ASC')
+//                ->setMaxResults(10)
+//                ->getQuery()
+//                ->getResult()
+//            ;
+//        }
 
     //    public function findOneBySomeField($value): ?Participant
     //    {
