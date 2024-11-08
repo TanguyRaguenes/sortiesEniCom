c8ae557d17a44190a228ebe6dea7d9bfc788f622
Tanguy <tanguy.raguenes@gmail.com>
2024-11-08 17:03:01 +0100

    Tanguy_List page

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,13 +10,12 @@ module.exports = {
         'PressStart2P': ['PressStart2P', 'sans-serif'],
       }
     },
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+    },
   },
   plugins: [],
-  // screens: {
-  //   'sm': '640px',
-  //   'md': '768px',
-  //   'lg': '1024px',
-  //   'xl': '1280px',
-  //   '2xl': '1536px',
-  // }
 }
